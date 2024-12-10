a5c6bf53438d05eeabf8444493c5494baf40d94b
Joel Setterberg <joel.setterberg.2@volvocars.com>
2024-12-10 15:22:13 +0000

    Testing another approach

--- day06/day06_2.go
+++ day06/day06_2.go
@@ -4,134 +4,7 @@ import (
 	"AoC-2024/utils"
 	"slices"
 	"strings"
-	"sync"
-	"sync/atomic"
 )
-
-func Day06_2() {
-	input := strings.Split(utils.GetData(6, utils.Real), "\n")
-	start := Pos{0, 0}
-	board := [][]string{}
-	initialize(&start, &board, &input)
-
-	var count int32 = 0
-	walk(&start, &board, &count)
-
-	// for _, e := range board {
-	// 	fmt.Println(e)
-	// }
-
-	println(count)
-}
-
-const maxSteps = 100000
-
-func testLoop(start Pos, dir Pos, flow map[Pos][]Pos, board [][]string, count *int32, wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	next := Pos{
-		start.x + dir.x,
-		start.y + dir.y,
-	}
-	nextTile := getValid(next, board)
-	if nextTile != nil {
-		*nextTile = "#"
-		dir = directions[dir]
-	}
-
-	tile := getValid(start, board)
-	step := 0
-	for tile != nil {
-		if slices.Contains(flow[start], dir) || step >= maxSteps {
-			atomic.AddInt32(count, 1)
-			break
-		}
-
-		// Next step
-		next := Pos{
-			start.x + dir.x,
-			start.y + dir.y,
-		}
-
-		// Check if should turn
-		nextTile := getValid(next, board)
-		for nextTile != nil && *nextTile == "#" {
-			dir = directions[dir]
-			next = Pos{
-				start.x + dir.x,
-				start.y + dir.y,
-			}
-			nextTile = getValid(next, board)
-		}
-
-		flow[start] = append(flow[start], dir)
-
-		// Move to next
-		start.x += dir.x
-		start.y += dir.y
-		tile = getValid(start, board)
-
-		step++
-	}
-}
-
-func walk(start *Pos, board *[][]string, count *int32) {
-	var wg sync.WaitGroup
-	dir := Pos{0, -1}
-	tile := getValid(*start, *board)
-	flow := make(map[Pos][]Pos)
-
-	for tile != nil {
-		// *tile = "x"
-		if slices.Contains(flow[*start], dir) {
-			break
-		}
-
-		// Next step
-		next := Pos{
-			start.x + dir.x,
-			start.y + dir.y,
-		}
-
-		// Check if should turn
-		nextTile := getValid(next, *board)
-		for nextTile != nil && *nextTile == "#" {
-			dir = directions[dir]
-			next = Pos{
-				start.x + dir.x,
-				start.y + dir.y,
-			}
-			nextTile = getValid(next, *board)
-		}
-
-		// Copy Flow
-		newFlow := make(map[Pos][]Pos, len(flow))
-		for key, value := range flow {
-			copiedSlice := make([]Pos, len(value))
-			copy(copiedSlice, value)
-			newFlow[key] = copiedSlice
-		}
-
-		// Copy board
-		newBoard := make([][]string, len(*board))
-		for i := range *board {
-			newBoard[i] = make([]string, len((*board)[i]))
-			copy(newBoard[i], (*board)[i])
-		}
-
-		wg.Add(1)
-		go testLoop(*start, dir, newFlow, newBoard, count, &wg)
-
-		flow[*start] = append(flow[*start], dir)
-
-		// Move to next
-		start.x += dir.x
-		start.y += dir.y
-		tile = getValid(*start, *board)
-	}
-
-	wg.Wait()
-}
 
 func initialize(start *Pos, board *[][]string, input *[]string) {
 	for y, line := range *input {
@@ -142,4 +15,88 @@ func initialize(start *Pos, board *[][]string, input *[]string) {
 		row := strings.Split(line, "")
 		*board = append(*board, row)
 	}
+}
+
+const maxSteps = 100000
+
+func walk(start *Pos, dir Pos, board [][]string) ([]Pos, int32) {
+	path := []Pos{}
+	flow := make(map[Pos][]Pos)
+	tile := getValid(*start, board)
+	steps := 0
+	for tile != nil {
+		if slices.Contains(flow[*start], dir) ||
+			steps >= maxSteps {
+			return path, 1
+		}
+
+		next := Pos{
+			start.x + dir.x,
+			start.y + dir.y,
+		}
+		nextTile := getValid(next, board)
+		for nextTile != nil && *nextTile == "#" {
+			dir = directions[dir]
+			next = Pos{
+				start.x + dir.x,
+				start.y + dir.y,
+			}
+			nextTile = getValid(next, board)
+		}
+
+		path = append(path, *start)
+		flow[*start] = append(flow[*start], dir)
+
+		start.x += dir.x
+		start.y += dir.y
+		tile = getValid(*start, board)
+		steps++
+	}
+
+	// for _, e := range board {
+	// 	fmt.Println(e)
+	// }
+	// fmt.Println()
+
+	return path, 0
+}
+
+func Day06_2() {
+	input := strings.Split(utils.GetData(6, utils.Real), "\n")
+	start := Pos{0, 0}
+	board := [][]string{}
+	initialize(&start, &board, &input)
+
+	upDir := Pos{0, -1}
+	path, _ := walk(&start, upDir, board)
+
+	var count int32 = 0
+
+	for i := 0; i < len(path)-1; i++ {
+		cur := path[i]
+		next := path[i+1]
+		dir := Pos{
+			next.x - cur.x,
+			next.y - cur.y,
+		}
+
+		newBoard := make([][]string, len(board))
+		for i := range board {
+			newBoard[i] = make([]string, len((board)[i]))
+			copy(newBoard[i], (board)[i])
+		}
+
+		// X and Y is mixed.
+		newBoard[next.y][next.x] = "#"
+
+		_, loop := walk(&cur, dir, newBoard)
+		count += loop
+	}
+
+	// for _, e := range board {
+	// 	fmt.Println(e)
+	// }
+	// fmt.Println()
+
+	println(count)
 }
